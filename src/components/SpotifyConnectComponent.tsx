c14444a7940fc8081c99fccda1fdd10e0cebd78c
ErickCif <erick@air.ai>
2023-02-19 21:20:09 -0500

    Still going on Spotify testing page, love react children!

--- src/components/SpotifyConnectComponent.tsx
+++ src/components/SpotifyConnectComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SpotifyWebApi from "spotify-web-api-node";
+import {useState, useEffect} from "react";
 
 type Props = {
     client_id: string;
@@ -16,19 +17,22 @@ export async function SpotifyConnectComponent(props: Props){
        // redirectUri: 'http://localhost:3000/api/auth/callback/spotify'
     });
 
+    const[ test, setTest ] = useState(null);
+    useEffect(() = > {})
+
+
     const data = await api.clientCredentialsGrant();
     api.setAccessToken(data.body.access_token);
 
     const query = "Wesley's Theory";
     const result = await api.searchTracks(query);
-    const track = result.body.tracks.items[0];
-
+    const track = result.body.tracks?.items[0];
 
     return(
         <div className="flex-col items-center">
-            <h1>{track.name}</h1>
-            <p>{track.artists[0].name}</p>
-            <img src={track.album.images[0].url} alt="Album Cover"/>
+            <h1>{track?.name}</h1>
+            <p>{track?.artists[0].name}</p>
+            <img src={track?.album.images[0].url} alt="Album Cover"/>
         </div>
     )
 }
--- src/pages/spotify/spotify-testing.tsx
+++ src/pages/spotify/spotify-testing.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import {SpotifyConnectComponent} from "components/components/SpotifyConnectComponent";
+import { useState, useEffect} from "react";
 
-export default function spotifyTesting() : JSX.Element {
-    return(
+export default function spotifyTesting(){
+
+    const client = "2bf2dbe207264086aa7a81be7f9be525";
+    const secret = "1832b279215f41d9b4a5520b6facf35c";
+
+    const props = {
+        client_id: client,
+        secret_id: secret,
+    }
+
+    //const spotifyConnect = await SpotifyConnectComponent(props);
+
+    const [ spotifyConnect, setSpotifyConnect ] = useState<React.ReactNode>(null);
+    useEffect(() => {
+        async function getSpotifyConnect(){
+            const output = await SpotifyConnectComponent(props);
+            setSpotifyConnect(output);
+        }
+        getSpotifyConnect()
+    }, []);
+
+    return (
         <div>
-            <SpotifyConnectComponent
-                client_id={"2bf2dbe207264086aa7a81be7f9be525"}
-                secret_id={"1832b279215f41d9b4a5520b6facf35c"}/>
+            {spotifyConnect}
         </div>
     )
 }
