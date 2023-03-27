cca445b8d1e6397dc0b64fe1035421d91eb3c3c3
ErickCif <erick@air.ai>
2023-03-27 15:12:52 -0400

    build.yml and test fixes

--- tests/spotify.test.ts
+++
@@ -1,8 +0,0 @@
-import {expect, jest, test} from '@jest/globals';
-import {spotifyWebApi} from "../src/lib/spotifyClient";
-
-test('spotify client created', () => {
-    expect(spotifyWebApi).toBeDefined();
-})
-
-
--- tests/supabase.test.ts
+++
@@ -1,8 +0,0 @@
-import { defaults } from 'jest-config'
-import {expect, jest, test} from '@jest/globals';
-import {supabase} from "../src/lib/supabaseClient";
-
-test('supabase client created', async () => {
-    expect(supabase).toBeDefined();
-})
-
